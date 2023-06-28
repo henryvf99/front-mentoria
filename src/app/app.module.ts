745974af4021f6d1ac5f17734c4b54315efa6ff7
ElvisCA01 <mark.nicols@hotmail.com>
2023-06-28 18:41:20 -0500

    Agregados: Servicios/LoginPage/RegisterPage/Guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+import {FormsModule} from "@angular/forms";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {HttpClientModule} from "@angular/common/http";
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatSnackBarModule,
+        HttpClientModule,
+
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {RegisterService} from "../../services/registerService/register.service";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  public user = {
+    username: '',
+    password: '',
+    email: '',
+  }
+  constructor(private registerService:RegisterService){
+  }
+
+  register(){
+    if(this.user.username == '' || this.user.username == null){
+      alert("El nombre de usuario es requerido");
+      return;
+    }
+    this.registerService.añadirUsuario(this.user)
+      .subscribe(res=>{
+        console.log("Registrado!")
+      },error => {
+        console.log(error);
+      })
+  }
+
+
+  ngOnInit(): void {
+
+  }
+}
